f938ee527a2ac0839909cfbd16e5be26f8f9d6d8
salim <jimsky699@gmail.com>
2023-01-06 17:24:53 +0100

    crypt and check user password

--- controlers/user.controller.js
+++ controlers/user.controller.js
@@ -1,7 +1,113 @@
 const User = require('../models/user.model');
 const jwtUtils = require('../utils/jwt.utils');
+const bcrypt = require('bcrypt');
 
-const maxAge = 86400 * 1000;
+
+const maxAge = 864e5; // un jour
+
+module.exports.signIn = (req, res) => {
+    let email = req.body.email
+    let password = req.body.password
+
+    User.findOne({ email: email }).then(
+        (users) => {
+            if (users !== null) {
+                bcrypt.compare(password, users.password, (errBcrypt, resBcrypt) => {
+                    if (resBcrypt) {
+                        const token = jwtUtils.generateTokenForUser(users, maxAge);
+                        res.cookie("jwt", token)
+                        res.status(200).json({
+                            type: "success",
+                            message: "Connextion réussi !",
+                            data: {
+                                userId: users._id,
+                                email: users.email,
+                                token: token
+                            },
+                        });
+                    } else {
+                        res.status(403).json({
+                            type: "error",
+                            message: "Mot de passe invalide!",
+                            data: {},
+                        });
+                    }
+
+                })
+            } else {
+                res.status(404).json({
+                    type: "warning",
+                    message: "Email ou Mot de passe incorrect !",
+                    data: {},
+                });
+            }
+        }).catch((error) => {
+            res.status(400).json({
+                type: "error",
+                message: "une erreur s'est produite! reéssayer plus tard...",
+                data: {},
+            });
+            console.log(error);
+        });
+}
+
+module.exports.signUp = (req, res, _) => {
+
+
+    User.countDocuments({ email: req.body.email }, (err, count) => {
+        if (count > 0) {
+            res.status(400).json({
+                type: "warning",
+                message: "cette adresse Email a déjà un compte. utiliser une autre adresse ou connecter vous",
+                data: {},
+            });
+        } else {
+            bcrypt.hash(req.body.password, 5, (err, bcryptedPassword) => {
+                const users = new User({
+                    fullName: req.body.fullName,
+                    email: req.body.email,
+                    speciality: req.body.speciality,
+                    password: bcryptedPassword,
+                });
+
+                users.save().then((user) => {
+                    res.status(201).json({
+                        type: "success",
+                        message: "votre compte a été crée avec succès connecter vous!",
+                        data: user.email,
+                    });
+                }).catch((error) => {
+                    res.status(500).json({
+                        type: "error",
+                        message: "impossible d'ajouter l'utilisateur. reessayer plus tard...",
+                        data: {},
+                    });
+                    console.log(error);
+                });
+            })
+        }
+    });
+
+}
+
+module.exports.logOut = (req, res) => {
+    try {
+        res.cookie("jwt", "", { maxAge: 1 })
+        res.redirect("/")
+        res.status(200).json({
+            type: "success",
+            message: "Vous étes actuellement déconnecté",
+            data: {},
+        });
+    } catch (error) {
+        res.status(400).json({
+            type: "error",
+            message: "Erreur inattendue. reéssayer plus tard...",
+            data: {},
+        });
+    }
+}
+
 module.exports.get = (req, res) => {
     let user = res.locals.user;
     if (user) {
@@ -31,57 +137,6 @@ module.exports.get = (req, res) => {
     }
 }
 
-module.exports.signIn = (req, res) => {
-    let email = req.body.email
-
-    User.findOne({ email: email }).then(
-        (users) => {
-            if (users !== null) {
-                const token = jwtUtils.generateTokenForUser(users, maxAge);
-                res.cookie("jwt", token)
-                res.status(200).json({
-                    type: "success",
-                    message: "Connextion réussi !",
-                    data: {
-                        userId: users._id,
-                        email: users.email,
-                        token: token
-                    },
-                });
-            } else {
-                res.status(404).json({
-                    type: "warning",
-                    message: "Email ou Mot de passe incorrect !",
-                    data: {},
-                });
-            }
-        }).catch((error) => {
-            res.status(400).json({
-                type: "error",
-                message: "une erreur s'est produite! reéssayer plus tard...",
-                data: {},
-            });
-            console.log(error);
-        });
-}
-module.exports.logOut = (req, res) => {
-    try {
-        res.cookie("jwt", "", { maxAge: 1 })
-        res.redirect("/")
-        res.status(200).json({
-            type: "success",
-            message: "Vous étes actuellement déconnecté",
-            data: {},
-        });
-    } catch (error) {
-        res.status(400).json({
-            type: "error",
-            message: "Erreur inattendue. reéssayer plus tard...",
-            data: {},
-        });
-    }
-}
-
 module.exports.getOne = (req, res) => {
     User.findOne({ _id: req.params.id }).then(
         (users) => {
@@ -98,42 +153,6 @@ module.exports.getOne = (req, res) => {
             });
             console.log(error);
         });
-}
-
-
-module.exports.signUp = (req, res, _) => {
-    const users = new User({
-        fullName: req.body.fullName,
-        email: req.body.email,
-        speciality: req.body.speciality,
-        password: req.body.password,
-    });
-
-    User.countDocuments({ email: req.body.email }, function (err, count) {
-        if (count > 0) {
-            res.status(400).json({
-                type: "warning",
-                message: "cette adresse Email a déjà un compte. utiliser une autre adresse ou connecter vous",
-                data: {},
-            });
-        } else {
-            users.save().then((user) => {
-                res.status(201).json({
-                    type: "success",
-                    message: "votre compte a été crée avec succès connecter vous!",
-                    data: user.email,
-                });
-            }).catch((error) => {
-                res.status(500).json({
-                    type: "error",
-                    message: "impossible d'ajouter l'utilisateur. reessayer plus tard...",
-                    data: {},
-                });
-                console.log(error);
-            });
-        }
-    });
-
 }
 
 module.exports.delete = (req, res) => {
