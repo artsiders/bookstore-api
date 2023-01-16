55e648c4136c5cfda161b794965a6cd53065affb
salim <jimsky699@gmail.com>
2023-01-16 14:45:54 +0100

    fetch data without password

--- models/user.model.js
+++ models/user.model.js
@@ -4,6 +4,6 @@ const userSchema = mongoose.Schema({
     fullName: { type: String, required: true },
     email: { type: String, required: true },
     speciality: { type: String, required: true },
-    password: { type: String, required: true },
+    password: { type: String, required: true, select: false },
 });
 module.exports = mongoose.model('User', userSchema);
